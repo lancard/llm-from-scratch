96b2b8fdcdfc8c0d2dfdd53f9108848e76a12e62
Sungho Kim <worstrocker@gmail.com>
2024-10-24 17:05:40 +0900

    구조화

--- llm_training.py
+++ llm_training.py
@@ -13,12 +13,15 @@ if __name__ == '__main__':
     tokenizer = CustomTokenizer('./my_finetuned_tokenizer')
 
     print("1. loading dataset")
-    dataset = load_dataset("csv", data_files="formatted_data.csv")[
-        "train"]  # .select(range(10))
+    dataset = load_dataset("csv", data_files="./data/train2.csv")["train"]
     texts = [d['text'] for d in dataset]
+
+    dataset = load_dataset("json", data_files="./data/train.jsonl")["train"]
+    texts += [f"[BOS] {d['instruction']} [PAD] {d['output']} [EOS]" for d in dataset]
+
     dataloader = LlmUtil.create_data_loader(
         texts, tokenizer, max_length=1000, batch_size=2)
-    print(f"Len: {len(texts)}")
+    print(f" - data length: {len(texts)}")
     print(" - complete")
 
     # 어휘 사전 생성
@@ -27,15 +30,15 @@ if __name__ == '__main__':
     print(" - complete")
 
     # 모델, 옵티마이저 준비
-    if os.path.exists('./my_finetuned_model'):
-        model = LlmUtil.load_gpt2_model('./my_finetuned_model')
-    else:
-        model = LlmUtil.create_gpt2_model(vocab_size=len(tokenizer.vocab))
+    # if os.path.exists('./my_finetuned_model'):
+    #    model = LlmUtil.load_gpt2_model('./my_finetuned_model')
+    # else:
+    model = LlmUtil.create_gpt2_model(vocab_size=len(tokenizer.vocab))
     optimizer = LlmUtil.create_optimizer(model)
 
     # 학습 시작
     print("3. begin training")
-    LlmUtil.train_gpt2(model, dataloader, optimizer, device, num_epochs=10)
+    LlmUtil.train_gpt2(model, dataloader, optimizer, device, num_epochs=3)
     print(" - complete")
 
     print("4. saving model")
