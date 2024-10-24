7823669c1663446a3de524bdb8612c005fc940a2
Sungho Kim <worstrocker@gmail.com>
2024-10-24 11:50:44 +0900

    update

--- custom_tokenizer.py
+++ custom_tokenizer.py
@@ -1,16 +1,44 @@
 from kiwipiepy import Kiwi
+import os
+import json
 
 
 class CustomTokenizer:
-    def __init__(self, special_tokens=["[PAD]", "[UNK]", "[BOS]", "[EOS]"]):
+    def __init__(self, dir_path=''):
         self.kiwi = Kiwi()
-        for st in special_tokens:
-            self.kiwi.add_user_word(st)
-        self.special_tokens_set = set(special_tokens)
-        self.special_tokens = special_tokens
-        self.vocab = {token: idx for idx, token in enumerate(special_tokens)}
-        self.inverse_vocab = {idx: token for idx,
-                              token in enumerate(special_tokens)}
+        self.load_config(dir_path)
+
+    def _generate_related_data(self):
+        self.special_tokens_set = set(self.special_tokens)
+        self.inverse_vocab = {self.vocab[token]: token for token in self.vocab}
+
+    def load_config(self, dir_path, special_tokens=["[PAD]", "[UNK]", "[BOS]", "[EOS]"]):
+        if dir_path == '':
+            for st in special_tokens:
+                self.kiwi.add_user_word(st)
+            self.special_tokens = special_tokens
+            self.vocab = {token: idx for idx,
+                          token in enumerate(special_tokens)}
+            self._generate_related_data()
+            return
+
+        # load config
+        file_path = os.path.join(dir_path, "config.json")
+
+        with open(file_path, 'r', encoding='utf-8') as json_file:
+            json_data = json.load(json_file)
+            self.special_tokens = json_data['special_tokens']
+            self.vocab = json_data['vocab']
+            self._generate_related_data()
+            
+
+    def save_config(self, dir_path):
+        os.makedirs(dir_path, exist_ok=True)
+
+        file_path = os.path.join(dir_path, "config.json")
+
+        with open(file_path, 'w', encoding='utf-8') as json_file:
+            json.dump({"special_tokens": self.special_tokens, "vocab": self.vocab}, json_file, ensure_ascii=False, indent=4)
 
     def build_vocab(self, texts):
         """텍스트 목록으로부터 어휘 사전을 생성합니다."""
@@ -49,7 +77,11 @@ class CustomTokenizer:
         tokens = [self.inverse_vocab.get(idx, "[UNK]") for idx in token_ids]
         if skip_special_tokens:
             tokens = [t for t in tokens if t not in self.special_tokens]
-        return "   ".join(tokens)
+        
+        tuple_list = []
+        for t in tokens:
+            tuple_list.append((t.split("/")[0], t.split("/")[1]))
+        return self.kiwi.join(tuple_list)
 
     @property
     def pad_token_id(self):
---
+++ llm_util.py
@@ -0,0 +1,87 @@
+import torch
+from transformers import GPT2LMHeadModel, GPT2Config
+from torch.utils.data import Dataset, DataLoader
+
+
+class TextDataset(Dataset):
+    def __init__(self, texts, tokenizer, max_length):
+        self.texts = texts
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.texts)
+
+    def __getitem__(self, idx):
+        text = self.texts[idx]
+        input_ids = self.tokenizer.encode(text, add_special_tokens=True)
+
+        # 패딩
+        input_ids = input_ids[:self.max_length] + \
+            [self.tokenizer.pad_token_id] * (self.max_length - len(input_ids))
+        attention_mask = [
+            1 if id != self.tokenizer.pad_token_id else 0 for id in input_ids]
+
+        return {"input_ids": torch.tensor(input_ids), "attention_mask": torch.tensor(attention_mask)}
+
+
+class LlmUtil:
+    # GPT2 모델 생성
+    def create_gpt2_model(vocab_size):
+        configuration = GPT2Config(vocab_size=vocab_size)
+        model = GPT2LMHeadModel(configuration)
+        return model
+
+    # 데이터 로더 설정
+    def create_data_loader(texts, tokenizer, max_length, batch_size):
+        dataset = TextDataset(texts, tokenizer, max_length)
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        return dataloader
+
+    # 옵티마이저 생성
+    def create_optimizer(model, lr=5e-5):
+        return torch.optim.AdamW(model.parameters(), lr=lr)
+
+    # 학습 함수 정의
+    def train_gpt2(model, dataloader, optimizer, device, num_epochs):
+        model = model.to(device)
+        model.train()
+        for epoch in range(num_epochs):
+            for batch in dataloader:
+                optimizer.zero_grad()
+                input_ids = batch['input_ids'].to(device)
+                attention_mask = batch['attention_mask'].to(device)
+
+                outputs = model(input_ids=input_ids,
+                                attention_mask=attention_mask, labels=input_ids)
+                loss = outputs.loss
+                print(f"loss: {loss}")
+                loss.backward()
+                optimizer.step()
+
+            print(f"Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}")
+
+    # 텍스트 생성 함수
+    def generate_text(model: GPT2LMHeadModel, tokenizer, prompt, max_length=50, num_return_sequences=1):
+        model.eval()
+        input_ids = torch.tensor(
+            [tokenizer.encode(prompt, add_special_tokens=True)]).to(model.device)
+
+        with torch.no_grad():
+            output = model.generate(
+                input_ids,
+                max_length=max_length,
+                num_return_sequences=num_return_sequences,
+                no_repeat_ngram_size=2,
+                repetition_penalty=2.0,
+                top_p=0.95,
+                temperature=1.0,
+                do_sample=True,
+                top_k=50,
+                eos_token_id=tokenizer.vocab["[EOS]"],
+                pad_token_id=tokenizer.pad_token_id
+            )
+
+        generated_texts = [tokenizer.decode(
+            output_seq.tolist()) for output_seq in output]
+        return generated_texts
